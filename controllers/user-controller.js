6c9fc94792c6c6468bc4c79ff3d692e76c591105
DiegoServe <110145883+DiegoServe@users.noreply.github.com>
2022-12-06 16:39:28 -0500

    add, update, delete user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -26,7 +26,7 @@ const userController = {
       })
       .select("-__v")
       .then((dbUserData) => {
-        // if no user found, send 404
+        // if no user found
         if (!dbUserData) {
           res.status(404).json({ message: "No user found with this id!" });
           return;
@@ -37,4 +37,38 @@ const userController = {
         console.log(err);
         res.status(404).json(err);
       });
+  },
+
+   // add a user
+   addUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  // update a user by id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .select("-__v")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // delete user
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
   },
